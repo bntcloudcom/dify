03082adf9d90d25a92ce26ced940ee0f0d18a725
osnysouza <contato@osnysouza.com.br>
2025-05-09 18:46:50 -0300

    first commit

---
+++ web/app/(commonLayout)/apps/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { useContextSelector } from 'use-context-selector'
+import { useTranslation } from 'react-i18next'
+import { RiDiscordFill, RiGithubFill } from '@remixicon/react'
+import Link from 'next/link'
+import style from '../list.module.css'
+import Apps from './Apps'
+import AppContext from '@/context/app-context'
+import { LicenseStatus } from '@/types/feature'
+
+const AppList = () => {
+  const { t } = useTranslation()
+  const systemFeatures = useContextSelector(AppContext, v => v.systemFeatures)
+
+  return (
+    <div className='relative flex flex-col overflow-y-auto bg-background-body shrink-0 h-0 grow'>
+      <Apps />
+      {systemFeatures.license.status === LicenseStatus.NONE && <footer className='px-12 py-6 grow-0 shrink-0'>
+        <div className='flex items-center gap-2 mt-3'>
+        </div>
+      </footer>}
+    </div >
+  )
+}
+
+export default AppList
